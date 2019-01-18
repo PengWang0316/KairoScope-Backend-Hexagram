9104269f37dc34b23af2ed028c52ec1976966d6b
Kevin Wang <rocwang0316@gmail.com>
2019-01-17 20:48:52 -0800

    Fixed some JSON parsing bugs.

--- functions/update-hexagram.js
+++ functions/update-hexagram.js
@@ -8,10 +8,10 @@ const { promiseInsertResult } = require('../libs/MongoDBHelper');
 const cloudwatch = require('../libs/cloudwatch');
 const log = require('../libs/log');
 
-const handler = async (event, context) => {console.log(event.body.hexagram);
-  const { hexagram } = JSON.parse(event.body);console.log(hexagram);
+const handler = async (event, context) => {
+  const { hexagram } = JSON.parse(event.body);
   try {
-    if (!context.user.role || context.user.role !== process.env.ADMINISTRATOR_ROLE * 1) throw new Error(`Invaild user ${JSON.stringify(context.user)}`);
+    if (!context.user.role || context.user.role * 1 !== process.env.ADMINISTRATOR_ROLE * 1) throw new Error(`Invaild user ${JSON.stringify(context.user)}`);
     await cloudwatch.trackExecTime('MongoDbInsertLatancy', () => promiseInsertResult(db => {
       const newHexagram = { ...hexagram };
       delete newHexagram._id;
